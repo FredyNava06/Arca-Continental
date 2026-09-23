0ab28c7791d2c75ebed527d84b455e7bea2e312d
FredyNava06 <alfredonavam02@gmail.com>
2026-09-23 10:15:25 -0600

    Add files via upload

--- AlfredoNava.py
+++ AlfredoNava.py
@@ -165,19 +165,18 @@ if uploaded_file is not None:
             sheet_name=Hoja_Merma
         )
 
-        df["Fecha"] = pd.to_datetime(
-            df["Fecha"],
-            format="%d/%m/%Y",
+        df["Fecha de registro"] = pd.to_datetime(
+            df["Fecha de registro"], 
+            format="%d/%m/%Y", 
             errors="coerce"
         )
 
+        df["Año"] = df["Fecha de registro"].dt.isocalendar().year
+
+        df["Semana"] = df["Fecha de registro"].dt.isocalendar().week
+    
         df["Kg merma"] = pd.to_numeric(
             df["Kg merma"],
-            errors="coerce"
-        )
-
-        df["Costo"] = pd.to_numeric(
-            df["Costo"],
             errors="coerce"
         )
 
@@ -190,8 +189,8 @@ if uploaded_file is not None:
             .str.title()
         )
 
-        df["Motivo merma"] = (
-            df["Motivo merma"]
+        df["Motivo de merma"] = (
+            df["Motivo de merma"]
             .str.strip()
             .str.normalize("NFKD")
             .str.encode("ascii", errors="ignore")
@@ -275,13 +274,13 @@ if uploaded_file is not None:
             # ---------------------------------------------
 
             top_motivos = (
-                m.groupby("Motivo merma")["Kg merma"]
+                m.groupby("Motivo de merma")["Kg merma"]
                 .sum()
                 .nlargest(3)
             )
 
             sin_clasificar_mot = (
-                m[m["Motivo merma"].isna()]
+                m[m["Motivo de merma"].isna()]
                 ["Kg merma"]
                 .sum()
             )
@@ -423,7 +422,7 @@ if uploaded_file is not None:
             # ─────────────────────────────────────────────
 
             data_mot = (
-                m.groupby("Motivo merma")["Kg merma"]
+                m.groupby("Motivo de merma")["Kg merma"]
                 .sum()
                 .nlargest(5)
                 .sort_values(ascending=True)
